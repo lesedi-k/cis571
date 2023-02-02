f13dcebe8688ea43b8e76e97cf8a484262f88bd8
lesedi-k <97709988+lesedi-k@users.noreply.github.com>
2023-02-02 13:59:19 -0500

    testing one iter with for loop

--- lab2a-div/lc4_divider.v
+++ lab2a-div/lc4_divider.v
@@ -16,8 +16,22 @@ module lc4_divider(input  wire [15:0] i_dividend,
                    output wire [15:0] o_quotient);
 
       /*** YOUR CODE HERE ***/
+      //handle zero case?
 
-endmodule // lc4_divider
+      wire [15:0] remainder = 16'b0;
+      wire [15:0] quotient = 16'b0;
+
+      genvar i;
+      for (i = 0; i < 16; i = i+1) begin
+            lc4_divider_one_iter m(.i_dividend(i_dividend << i),
+                  .i_divisor(i_divisor),
+                  .i_remainder(remainder),
+                  .i_quotient(quotient),
+                  .o_remainder(o_remainder),
+                  .o_quotient(o_quotient));
+      end
+
+endmodule 
 
 module lc4_divider_one_iter(input  wire [15:0] i_dividend,
                             input  wire [15:0] i_divisor,
@@ -27,24 +41,12 @@ module lc4_divider_one_iter(input  wire [15:0] i_dividend,
                             output wire [15:0] o_remainder,
                             output wire [15:0] o_quotient);
 
-      /*** YOUR CODE HERE ***/
-      
-      // remainder = (remainder << 1) | ((dividend >> 15) & 0x1);
-      //   if (remainder < divisor) {
-      //       quotient = (quotient << 1);
-      //   } else {
-      //       quotient = (quotient << 1) | 0x1;
-      //       remainder = remainder - divisor;
-      //   }
-
-      //   dividend = dividend << 1;
-
       wire [15:0] remainder = (i_remainder <<< 1) | ((i_dividend >>> 15) & 16'd1);
       assign o_quotient = (remainder < i_divisor) ? (i_quotient <<< 1) : ((i_quotient <<< 1) | 16'd1 ) ;
       assign o_remainder = (remainder >= i_divisor) ? (remainder - i_divisor) : remainder; 
-      assign o_dividend = i_dividend << 1;
+      //assign o_dividend = i_dividend << 1; 
 
-      //Questions: should we use  subtractor module?
-      //comparison module?
+      // Questions: should we use  subtractor module?
+      // comparison module?     
 endmodule
 
