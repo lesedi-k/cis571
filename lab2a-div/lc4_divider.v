a1d74670a3b33cefc63474a1dfd159129581f20f
lesedi-k <97709988+lesedi-k@users.noreply.github.com>
2023-02-02 13:03:01 -0500

    changes to lc4_divider_one_iter
    dsdfs

--- lab2a-div/lc4_divider.v
+++ lab2a-div/lc4_divider.v
@@ -1,4 +1,11 @@
-/* TODO: INSERT NAME AND PENNKEY HERE */
+//HOLD
+
+/* 
+      TODO: INSERT NAME AND PENNKEY HERE 
+      NAMES: Lesedi Kereteletswe, 
+      PENNKEYS: lesedik
+
+*/
 
 `timescale 1ns / 1ps
 `default_nettype none
@@ -21,5 +28,28 @@ module lc4_divider_one_iter(input  wire [15:0] i_dividend,
                             output wire [15:0] o_quotient);
 
       /*** YOUR CODE HERE ***/
+      
+      // remainder = (remainder << 1) | ((dividend >> 15) & 0x1);
+      //   if (remainder < divisor) {
+      //       quotient = (quotient << 1);
+      //   } else {
+      //       quotient = (quotient << 1) | 0x1;
+      //       remainder = remainder - divisor;
+      //   }
 
+      //   dividend = dividend << 1;
+
+      wire [15:0] remainder = (i_remainder << 1) | ((i_dividend >> 15) & 16'd1);
+
+      begin
+            if (remainder < i_divisor)
+                  assign o_quotient = i_quotient << 1;
+            else 
+                  assign o_quotient = (i_quotient << 1) | 16'd1;
+                  assign o_remainder = remainder - i_divisor; 
+      end 
+      assign o_dividend = i_dividend << 1;
+
+      //Questions: should we use  subtractor module?
 endmodule
+
